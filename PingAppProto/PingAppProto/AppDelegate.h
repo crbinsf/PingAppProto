cd022face9e0bd15a3962923d44d843c65712309
crbinsf <crb1973@mac.com>
2014-10-19 12:57:17 -0700

    Initial Commit of Project
    Initial Commit of PingProtoApp

---
+++ PingAppProto/PingAppProto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PingAppProto
+//
+//  Created by Clarke Bishop on 10/19/14.
+//  Copyright (c) 2014 Clarke Bishop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PingAppProto/PingAppProto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PingAppProto
+//
+//  Created by Clarke Bishop on 10/19/14.
+//  Copyright (c) 2014 Clarke Bishop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
